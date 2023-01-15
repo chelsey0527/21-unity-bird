a206668c246869f6c3a77204b27b7ba80ac257ed
李沁珊 Chelsey Lee <49125778+chelsey0527@users.noreply.github.com>
2023-01-15 22:34:34 +0800

    initial commit

---
+++ Assets/gameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameManager : MonoBehaviour
+{
+    public GameObject GameOverCanvas;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        GameOverCanvas.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    public void GameOver()
+    {
+        GameOverCanvas.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void replay()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
